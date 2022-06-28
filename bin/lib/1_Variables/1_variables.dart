23d558145d838445c9d93bf17fb3f50295d260b6
Gabriel Donato <gabrieldonato@brasilcard.net>
2022-06-28 20:18:22 -0300

    Feat: Variables

---
+++ bin/lib/1_Variables/1_variables.dart
@@ -0,0 +1,24 @@
+void main() {
+  var age = 19;
+  var iphoneValue = 12.000;
+  num test = 12;
+  num test2 = 12.5;
+
+  //camelCase
+  var courseName = 'Academia do Flutter';
+
+  //snake_case
+  var course_name = 'Academia do Flutter';
+  course_name = 'New name';
+
+  dynamic tryNull = null;
+
+  Object objeto = 12;
+  objeto = '12';
+  objeto = tryNull;
+  // objeto = null; not accept
+
+  dynamic dinamico = 12;
+  dinamico = 'dinamico';
+  dinamico = null;
+}
